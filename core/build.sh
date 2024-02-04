141f5223614992c31b91ace76730bec62c201656
Yeonwoo Cho <wooisso@gmail.com>
2024-02-05 00:07:54 +0900

    make buildscript for wasm

--- core/build.sh
+++ core/build.sh
@@ -7,12 +7,18 @@ if [[ $# -eq 0 ]]; then
     exit 0
 fi
 
+mkdir -p build
+
 if [[ $1 == "release" ]]; then
     emcmake cmake . -DCMAKE_BUILD_TYPE=Release
     emmake make
+    #emcc -O0 -g wasm/TravelTogether.wasm -o build/TravelTogether.js
 elif [[ $1 == "debug" ]]; then
     emcmake cmake . -DCMAKE_BUILD_TYPE=Debug
     emmake make
+    #emcc -O3 wasm/TravelTogether.wasm -o build/TravelTogether.js
+elif [[ $1 == "clean" ]]; then
+    rm -rf build CMakeFiles CMakeCache.txt cmake_install.cmake Makefile
 else
     echo "Usage: build.sh [release|debug]"
 fi
